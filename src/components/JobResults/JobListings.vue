9de799aebb8b70a6b1a39190d2d2ed1112324e74
Fabrizio <fabrizio.nastri@flexup.org>
2023-06-20 10:29:40 +0200

    created job listings

--- src/components/JobResults/JobListings.vue
+++ src/components/JobResults/JobListings.vue
@@ -1,9 +1,20 @@
 <template>
-  <main class="flex-auto p-8 bg-brand-gray-2">JobListings</main>
+  <main class="flex-auto p-8 bg-brand-gray-2">
+    <ol>
+      <job-listing />
+      <job-listing />
+      <job-listing />
+    </ol>
+  </main>
 </template>
 
 <script>
+import JobListing from './JobListing.vue'
+
 export default {
-  name: 'JobListings'
+  name: 'JobListings',
+  components: {
+    JobListing
+  }
 }
 </script>
