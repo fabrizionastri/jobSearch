c80119c7680f2f0075730aa853c9ae06b4e72dcc
Fabrizio <fabrizio.nastri@flexup.org>
2023-06-22 14:02:38 +0200

    19.239 refactor JobListings tests

--- test/unit/components/JobResults/JobListings.test.js
+++ test/unit/components/JobResults/JobListings.test.js
@@ -6,33 +6,36 @@ import axios from 'axios'
 vi.mock("axios") // this mocks all methods of the axios library
 
 describe('JobListings.vue', async () => {
+  const createRoute = (queryParams) => ({
+    query: {
+      page: '5',
+      ...queryParams
+    }
+  })
+
+  const renderJobListings = ($route) => {
+    render(JobListings, {
+      global: {
+        mocks: { $route },
+        stubs: { RouterLink: RouterLinkStub }
+      }
+    })
+  }
+
   it('fetches jobs', () => {
     // this is the response we want axios to return
     axios.get.mockResolvedValue({ data: [] })
-    const $route = {
-      query: { page: '5' }
-    }
-    render(JobListings, {
-      global: {
-        mocks: { $route },
-        stubs: { RouterLink: RouterLinkStub }
-      }
-    })
+    const $route = createRoute()
+    renderJobListings($route)
     expect(axios.get).toHaveBeenCalledWith('http://localhost:3000/jobs')
   })
+
   it('displays a maximum of 10 jobs', async () => {
-    axios.get.mockResolvedValue({
-      data: Array(15).fill({}) // creates an array of 15 empty objects
-    })
-    const $route = {
-      query: { page: '1' }
-    }
-    render(JobListings, {
-      global: {
-        mocks: { $route },
-        stubs: { RouterLink: RouterLinkStub }
-      }
-    })
+    axios.get.mockResolvedValue({ data: Array(15).fill({}) }) // creates an array of 15 empty objects
+    const queryParams = { page: '1' } // We want to be on page 1, to test that we only get 10 jobs
+    const $route = createRoute(queryParams)
+    renderJobListings($route)
+
     const jobListings = await screen.findAllByRole('listitem') // find is async, so we need to await it
     expect(jobListings).toHaveLength(10)
   })
