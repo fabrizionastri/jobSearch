79d916d9d5c01ec5a569efd7128e855c80f0bc70
Fabrizio <fabrizio.nastri@flexup.org>
2023-06-24 11:42:35 +0200

    20.269 tested getJobs

--- src/api/getJobs.js
+++ src/api/getJobs.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 const getJobs = async () => {
   const baseUrl = import.meta.env.VITE_APP_API_URL
   const response = await axios.get(`${baseUrl}/jobs`)
-  return response
+  return response.data
 }
 
 export default getJobs
--- test/unit/api/getJobs.test.js
+++ test/unit/api/getJobs.test.js
@@ -1,12 +1,30 @@
 import axios from 'axios'
 
 import getJobs from '@/api/getJobs'
+import { beforeEach } from 'vitest'
 
 vi.mock('axios')
 
 describe('getJobs', () => {
-  it('should return jobs', async () => {
+  beforeEach(() => {
+    axios.get.mockResolvedValue({
+      data: [
+        { id: 1, title: 'Job 1' },
+        { id: 2, title: 'Job 2' }
+      ]
+    })
+  })
+
+  it('calls axios to fetch jobs', async () => {
     await getJobs()
     expect(axios.get).toHaveBeenCalledWith('http://myfakeapi.com/jobs')
+  })
+
+  it('extracts jobs from response', async () => {
+    const response = await getJobs()
+    expect(response).toEqual([
+      { id: 1, title: 'Job 1' },
+      { id: 2, title: 'Job 2' }
+    ])
   })
 })
