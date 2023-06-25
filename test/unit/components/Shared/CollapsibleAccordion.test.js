6b7da852827c7e4b70c0458585c86ca37687712f
Fabrizio <fabrizio.nastri@flexup.org>
2023-06-25 17:27:37 +0200

    22.286 tested default prop content

--- test/unit/components/Shared/CollapsibleAccordion.test.js
+++ test/unit/components/Shared/CollapsibleAccordion.test.js
@@ -13,16 +13,34 @@ describe('CollapsibleAccordion', () => {
         header: 'My category'
       },
       slots: {
-        default: '<h3>My nested child</h3>'
-      }
+        default: 'My nested child',
+      },
     })
   })
-  it.only('it does not render child content before click', () => {
+  it('it does not render child content before click', () => {
     expect(screen.queryByText(/My nested child/i)).not.toBeInTheDocument()
   })
-  it.only('it does render child content after click', async () => {
+  it('it does render child content after click', async () => {
     const button = await screen.getByRole('button', { name: /My category/i })
     await userEvent.click(button)
     expect(screen.getByText(/My nested child/i)).toBeInTheDocument()
+  })
+})
+
+describe('when parent does not provide a custom child content', () => {
+  beforeEach(() => {
+    render(CollapsibleAccordion, {
+      global: {
+        stubs: ['font-awesome-icon']
+      },
+      props: {
+        header: 'My category'
+      }
+    })
+  })
+  it('renders default child content', async () => {
+    const button = await screen.getByRole('button', { name: /My category/i })
+    await userEvent.click(button)
+    expect(screen.getByText(/Default content/i)).toBeInTheDocument()
   })
 })
