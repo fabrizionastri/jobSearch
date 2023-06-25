fa5a2a8cc5e5be6ba4f7d3aa1eb83fd407a4af8b
Fabrizio <fabrizio.nastri@flexup.org>
2023-06-25 17:43:23 +0200

    22.287 refactor accordion test

--- test/unit/components/Shared/CollapsibleAccordion.test.js
+++ test/unit/components/Shared/CollapsibleAccordion.test.js
@@ -1,26 +1,31 @@
 import { render, screen } from '@testing-library/vue'
 import CollapsibleAccordion from '@/components/Shared/CollapsibleAccordion.vue'
 import userEvent from '@testing-library/user-event'
-import { beforeEach } from 'vitest'
+
+const renderCollapsibleAccordion = (config = {}) => {
+  render(CollapsibleAccordion, {
+    global: {
+      stubs: ['font-awesome-icon']
+    },
+    props: {
+      header: 'My category'
+    },
+    slots: {
+      default: '<h3>My nested child</h3>',
+    },
+    ...config
+  })
+}
 
 describe('CollapsibleAccordion', () => {
-  beforeEach(() => {
-    render(CollapsibleAccordion, {
-      global: {
-        stubs: ['font-awesome-icon']
-      },
-      props: {
-        header: 'My category'
-      },
-      slots: {
-        default: 'My nested child',
-      },
-    })
-  })
+  const props = { header: 'My category' }
+  const slots = { default: '<h3>My nested child</h3>' }
   it('it does not render child content before click', () => {
+    renderCollapsibleAccordion({ props, slots })
     expect(screen.queryByText(/My nested child/i)).not.toBeInTheDocument()
   })
   it('it does render child content after click', async () => {
+    renderCollapsibleAccordion({ props, slots })
     const button = await screen.getByRole('button', { name: /My category/i })
     await userEvent.click(button)
     expect(screen.getByText(/My nested child/i)).toBeInTheDocument()
@@ -28,17 +33,10 @@ describe('CollapsibleAccordion', () => {
 })
 
 describe('when parent does not provide a custom child content', () => {
-  beforeEach(() => {
-    render(CollapsibleAccordion, {
-      global: {
-        stubs: ['font-awesome-icon']
-      },
-      props: {
-        header: 'My category'
-      }
-    })
-  })
+  const props = { header: 'My category' }
+  const slots = {}
   it('renders default child content', async () => {
+    renderCollapsibleAccordion({ props, slots })
     const button = await screen.getByRole('button', { name: /My category/i })
     await userEvent.click(button)
     expect(screen.getByText(/Default content/i)).toBeInTheDocument()
