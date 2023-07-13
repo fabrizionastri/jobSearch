f28b2aa05ce58553b1de6437e13e4896c727f903
Fabrizio <fabrizio.nastri@flexup.org>
2023-07-13 08:17:18 +0200

    36:493 move clear filter to separate component

--- src/components/JobResults/JobFiltersSidebar/JobFiltersSidebar.vue
+++ src/components/JobResults/JobFiltersSidebar/JobFiltersSidebar.vue
@@ -2,20 +2,20 @@
 <template>
   <div class="flex flex-col p-4 bg-white border-r border-solid border-brand-gray-1 w-96">
     <section class="pb-5">
-      <div class="flex flex-row justify-between">
-        <h3 class="my-4 text-base font-semibold">What do you want to do ?</h3>
-        <div class="flex items-center text-sm">
-          <ActionButton text="Clear filters" type="secondary" @click="userStore.clearFilters" />
-        </div>
-      </div>
+      <TitleAndClearFilters />
+
       <JobTitleInput />
+
       <QualificationInput />
+
       <collapsible-accordion header="Organizations">
         <OrganizationsCheckBoxes />
       </collapsible-accordion>
+
       <collapsible-accordion header="JobTypes">
         <JobTypesCheckBoxes />
       </collapsible-accordion>
+
       <collapsible-accordion header="Degrees">
         <DegreesCheckBoxes />
       </collapsible-accordion>
@@ -26,18 +26,19 @@
 <script setup lang="ts">
 // import { onMounted } from 'vue'
 // import { useRoute } from 'vue-router'
-import ActionButton from '@/components/Shared/ActionButton.vue'
+
+import TitleAndClearFilters from './TitleAndClearFilters.vue'
 import CollapsibleAccordion from '@/components/Shared/CollapsibleAccordion.vue'
 import OrganizationsCheckBoxes from '@/components/JobResults/JobFiltersSidebar/OrganizationsCheckBoxes.vue'
 import JobTypesCheckBoxes from '@/components/JobResults/JobFiltersSidebar/JobTypesCheckBoxes.vue'
 import DegreesCheckBoxes from '@/components/JobResults/JobFiltersSidebar/DegreesCheckBoxes.vue'
 import JobTitleInput from '@/components/JobResults/JobFiltersSidebar/JobTitleInput.vue'
 import QualificationInput from '@/components/JobResults/JobFiltersSidebar/QualificationInput.vue'
-import { useUserStore } from '@/stores/user'
-const userStore = useUserStore()
 
-// onMounted = () => {
-//   const route = useRoute()
-//   // update the userStore.searchJobTitle with the query param
+// const parseSkillsSearchTerm = (searchTerm: string) => {
+//   const skills = searchTerm.split(',')
+//   return skills.map((skill) => skill.trim())
 // }
+
+// onMounted(parseSkillsSearchTerm)
 </script>
---
+++ src/components/JobResults/JobFiltersSidebar/TitleAndClearFilters.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="flex flex-row justify-between">
+    <h3 class="my-4 text-base font-semibold">What do you want to do ?</h3>
+    <div class="flex items-center text-sm">
+      <ActionButton text="Clear filters" type="secondary" @click="userStore.clearFilters" />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import ActionButton from '@/components/Shared/ActionButton.vue'
+import { useUserStore } from '@/stores/user'
+
+const userStore = useUserStore()
+</script>
